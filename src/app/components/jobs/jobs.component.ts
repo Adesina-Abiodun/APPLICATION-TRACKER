3321e32ec6721197285f8a4a8ba7fb28cebcd8a2
Adesina-Abiodun <101020483+Adesina-Abiodun@users.noreply.github.com>
2023-02-14 15:40:47 +0100

    working in progress

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { myApplication } from '../my-application.module';
 
 @Component({
   selector: 'app-jobs',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class JobsComponent {
 
+  Applications : myApplication[] = [
+
+    new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+     'Applied 3 days ago'),
+
+     new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+     'Applied 3 days ago'),
+
+    new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+     'Applied 3 days ago'),
+
+     new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+     'Applied 3 days ago'),
+
+     new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+     'Applied 3 days ago'),
+  ]
 }
--- src/app/components/my-application/my-application.component.ts
+++ src/app/components/my-application/my-application.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { myApplication } from '../my-application.module';
 
 @Component({
   selector: 'app-my-application',
   templateUrl: './my-application.component.html',
   styleUrls: ['./my-application.component.css']
 })
-export class MyApplicationComponent {
 
+export class MyApplicationComponent {
+    Applications : myApplication[] = [
+
+      new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+       'Applied 3 days ago'),
+
+       new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+       'Applied 3 days ago'),
+
+      new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+       'Applied 3 days ago'),
+
+       new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+       'Applied 3 days ago'),
+
+       new myApplication('../../../assets/unsplash_DBe29bswzpk.png', 'ABC Company', 'Senior Level Product Designer', 'United Arab Emirates', 1000000, ['figma', 'Sketch', 'Adobe', 'Illustrator', 'Photoshop', 'Imvision'],
+       'Applied 3 days ago'),
+    ]
 }
